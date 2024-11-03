a8b5c8a3b051b23f2f77cf62bb78e5aa97e05946
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-11-04 05:15:26 +0700

    feat: added simple API route

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+import json
+from odoo import http
 
+class OdooRestApiLab(http.Controller):
+    @http.route('/api/hello', type='json', auth='public')
+    def index(self, **kw):
+        data = {
+            'message': 'hello from odoo api!'
+        }
 
-# class OdooRestApiLab(http.Controller):
-#     @http.route('/odoo_rest_api_lab/odoo_rest_api_lab', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+        return http.Response(
+            json.dumps(data),
+            status=200,
+            mimetype='application/json'
+        )
 
 #     @http.route('/odoo_rest_api_lab/odoo_rest_api_lab/objects', auth='public')
 #     def list(self, **kw):
