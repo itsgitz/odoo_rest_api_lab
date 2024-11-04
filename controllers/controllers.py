d9589d97ce496428e8e2b917c639491548e92485
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-11-04 13:10:53 +0700

    fix: return response

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
 import json
+import logging
+
 from odoo import http
 
+_logger = logging.getLogger(__name__)
+
 class OdooRestApiLab(http.Controller):
-    @http.route('/api/hello', type='json', auth='public')
+    @http.route('/api/hello', auth='public', methods=['GET'])
     def index(self, **kw):
         data = {
             'message': 'hello from odoo api!'
         }
 
+        _logger.info('response data: %s', data)
+
         return http.Response(
             json.dumps(data),
             status=200,
-            mimetype='application/json'
+            mimetype='application/json' 
         )
 
 #     @http.route('/odoo_rest_api_lab/odoo_rest_api_lab/objects', auth='public')
